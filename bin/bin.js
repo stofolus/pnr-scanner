a7a443698242034543fb7ce7fd2c46109a13b79c
Andreas Cederström <andreas.cederstrom@gmail.com>
2018-04-27 00:59:09 +0200

    Added --fix option
    Right now the --fix option will find a random test pnr and replace the real one with that.
    
    Future improvement: Find replacement a bit smarter. Perhaps with the same birth date.

--- bin/bin.js
+++ bin/bin.js
@@ -4,6 +4,7 @@ const pkg = require(`../package.json`);
 const program = require(`commander`);
 const path = require(`path`);
 const Finder = require(`../lib/finder`);
+const Fixer = require(`../lib/fixer`);
 
 program
   .version(pkg.version)
@@ -13,6 +14,7 @@ program
     `-p, --pattern <pattern>`,
     `Glob pattern default is !(node_modules){,/**}`
   )
+  .option(`-f, --fix`, `Replace real pnrs with test pnrs`)
   .parse(process.argv);
 
 if (program.args.length !== 1) {
@@ -22,10 +24,22 @@ if (program.args.length !== 1) {
 
 console.time("pnr-scan");
 const directory = path.join(process.cwd(), program.args[0]);
-Finder.findPnrs(directory, program.pattern).forEach(item => {
+const result = Finder.findPnrs(directory, program.pattern);
+console.timeEnd("pnr-scan");
+
+if (program.fix && result.length > 0) {
+  console.time("pnr-fix");
+  Fixer.fixPnrs(result);
+  console.timeEnd("pnr-fix");
+}
+
+result.forEach(item => {
   console.log(item.file);
   item.results.forEach(hit => {
-    console.log(`    ${hit.pnr} on line ${hit.line}`);
+    if (hit.replacement) {
+      console.log(`    ${hit.pnr} => ${hit.replacement} on line ${hit.line}`);
+    } else {
+      console.log(`    ${hit.pnr} on line ${hit.line}`);
+    }
   });
 });
-console.timeEnd("pnr-scan");
