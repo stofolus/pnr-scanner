bf338ad95135a7e1494e9996c94e461bc24938ac
Christoffer Hasselberg <stofolus@gmail.com>
2018-05-08 09:17:28 +0200

    Added integration tests (#29)

---
+++ integration/integration.test.js
@@ -0,0 +1,89 @@
+const { execSync } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+
+let elapsedTime;
+let result;
+
+beforeAll(() => {
+  const start = process.hrtime();
+  const output = execSync(
+    `${path.join(
+      __dirname,
+      "../bin/bin.js"
+    )} mockdata/test1 mockdata/test2 mockdata/test3 mockdata/afile.js`,
+    { cwd: __dirname }
+  ).toString();
+  elapsedTime = process.hrtime(start);
+  result = parseResult(output);
+});
+
+describe("pnr-scanner", () => {
+  test("Should find pnrs in deeply nested files", () => {
+    expect(result).toContainEqual({
+      file: path.join(
+        __dirname,
+        "./mockdata/test1/deep/deeper/blackhole/main.java"
+      ),
+      results: ["121212-1212 on line 3"]
+    });
+  });
+
+  test("Should find pnrs in large files", () => {
+    expect(result).toContainEqual({
+      file: path.join(__dirname, "./mockdata/test3/hugefile.txt"),
+      results: ["201212121212 on line 1647"]
+    });
+  });
+
+  test("Should find in files supplied as arguments", () => {
+    expect(result).toContainEqual({
+      file: path.join(__dirname, "./mockdata/afile.js"),
+      results: ["20121212-1212 on line 3", "19121212-1212 on line 4"]
+    });
+  });
+
+  test("Should respect .gitignore files", () => {
+    expect(result).not.toContainEqual({
+      file: path.join(__dirname, "./mockdata/test2/ignore/pnr.txt"),
+      results: expect.any(Array)
+    });
+    expect(result).toContainEqual({
+      file: path.join(__dirname, "./mockdata/test2/data/data.txt"),
+      results: expect.any(Array)
+    });
+    expect(result).not.toContainEqual({
+      file: path.join(__dirname, "./mockdata/test2/data/ignore.txt"),
+      results: expect.any(Array)
+    });
+  });
+
+  test("Should ignore binary files", () => {
+    expect(result).not.toContainEqual({
+      file: path.join(__dirname, "./mockdata/test4/maxresdefault.jpg"),
+      results: expect.any(Array)
+    });
+  });
+
+  test("Should not find any extra results", () => {
+    expect(result.length).toBe(4);
+  });
+
+  test("Should have acceptable performance", () => {
+    expect(elapsedTime[0] / 1000 + elapsedTime[1] / 1000000).toBeLessThan(1000);
+  });
+});
+
+function parseResult(output) {
+  return output
+    .split("\n")
+    .filter(item => item.startsWith(__dirname) || item.startsWith("    "))
+    .reduce((acc, curr) => {
+      if (curr.startsWith(__dirname)) {
+        acc.push({ file: curr, results: [] });
+      } else {
+        acc[acc.length - 1].results.push(curr.replace("    ", ""));
+      }
+      return acc;
+    }, []);
+}
---
+++ integration/mockdata/test1/deep/deeper/blackhole/main.java
@@ -0,0 +1,5 @@
+public class Test {
+  static void main(String[] args){
+    System.out.println("Mr twelve was born 121212-1212");
+  }
+}
